c590b882227fdbd50d822853578eeacfda25fc3e
DhruvS28 <DhruvS28@users.noreply.github.com>
2018-11-26 15:05:27 +0000

    20181126T150503Z

--- mario.c
+++ mario.c
@@ -4,12 +4,13 @@
 int main(void)
 {
     int n, a = 0, b = 0, c = 0;
-do
-{
+
     printf("How many layers should the pyramid have?\n");
     n = get_int();
     int d = n;
 
+if (n >= 0 && n <= 23)
+{
     for (c = 0; n > c; n-- )
 {
     for (a = 0; a < n - 1 ; a++ )
@@ -38,6 +39,6 @@ do
         printf("\n");
         }
 }
+
 }
-while (n > -1 || n < 24);
 }
